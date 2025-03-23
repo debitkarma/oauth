d6daf85063e9c672c40dab8698fb266d51fe24e2
Debit <debitkarma@gmail.com>
2025-03-23 00:47:49 -0400

    added basic functions

---
+++ app.py
@@ -0,0 +1,10 @@
+import click
+import requests
+
+from requests import Response
+
+def check_article_exists(url:str) -> int | None:
+    pass
+
+def get_article_if_reachable(url:str) -> Response | None:
+    pass
